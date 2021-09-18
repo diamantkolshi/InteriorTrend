49e072d331cea31e40b8e7e3a33310de69866b01
Diamant <diamantkolshi@gmail.com>
2021-09-18 14:06:36 +0200

    fail test

--- spec/features/static_page_spec.rb
+++ spec/features/static_page_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe 'navigate' do
   it 'can be reached' do
     visit inertia_example_path
-    expect(page.status_code).to eq(200)
+    expect(page.status_code).to eq(201)
   end
 end
