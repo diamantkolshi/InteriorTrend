4ebff0f313b3e3e560402edf43c20e144e36d518
Diamant <diamantkolshi@gmail.com>
2021-09-21 23:00:46 +0200

    Create category model with subcategory itself ..

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_182521) do
+ActiveRecord::Schema.define(version: 2021_09_21_205514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "parent_id"
+    t.boolean "active", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["parent_id"], name: "index_categories_on_parent_id"
+  end
 
   create_table "cities", force: :cascade do |t|
     t.string "name", null: false
