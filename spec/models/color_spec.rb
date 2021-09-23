53e6d54485de62c3881ef5c8d76000f1405397dc
Diamant <diamantkolshi@gmail.com>
2021-09-23 23:17:29 +0200

    Add new user like company ..

--- spec/models/color_spec.rb
+++ spec/models/color_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Color, type: :model do
-  before do
-    binding.pry
-  end
-
   describe "Associations" do
     it { should have_many(:ingredient_colors) }
     it { should have_many(:ingredients).through(:ingredient_colors) }
