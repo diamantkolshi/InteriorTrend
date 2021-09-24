f82291892963a5d75a7b42f862d1994d576fb8bc
Diamant <diamantkolshi@gmail.com>
2021-09-24 22:15:39 +0200

    Add some categories with fixtures and fix test for category ..

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  before(:all) do
-    @main_category = Category.create(:name => 'Main')
-    @main_subcategory1 = Category.create(:name => 'Main Sub 1', parent_id: @main_category.id)
-    @main_subcategory2 = Category.create(:name => 'Main Sub 1', parent_id: @main_category.id)
-  end
 
   describe "Associations" do
     it { should have_many(:subcategories).with_foreign_key(:parent_id).class_name('Category') }
@@ -16,27 +11,33 @@ RSpec.describe Category, type: :model do
   describe "Validations" do
   end
 
-  describe "#subcategories" do
-    it "length should be 2" do
-      expect(@main_category.subcategories.count).to eq 2
+  describe 'actions' do
+    let(:main_category) { categories(:furniture) }
+    let(:main_subcategory1) { categories(:furniture_bed) }
+    let(:main_subcategory2) { categories(:furniture_chair) }
+
+    describe "#subcategories" do
+      it "length should be 2" do
+        expect(main_category.subcategories.count).to eq 4
+      end
+
+      it "display subcategories" do
+        expect(main_category.subcategories.limit(2)).to eq [main_subcategory1, main_subcategory2]
+      end
+
+      it "parent_id nil for main category" do
+        expect(main_category.parent_id).to be_nil
+      end
     end
 
-    it "display subcategories" do
-      expect(@main_category.subcategories).to eq [@main_subcategory1, @main_subcategory2]
-    end
+    describe '#parent' do
+      it "should return main catrgory from subcategory 1" do
+        expect(main_subcategory1.category).to eq main_category
+      end
 
-    it "parent_id nil for main category" do
-      expect(@main_category.parent_id).to be_nil
-    end
-  end
-
-  describe '#parent' do
-    it "should return main catrgory from subcategory 1" do
-      expect(@main_subcategory1.category).to eq @main_category
-    end
-
-    it "should return main catrgory from subcategory 2" do
-      expect(@main_subcategory2.category).to eq @main_category
+      it "should return main catrgory from subcategory 2" do
+        expect(main_subcategory2.category).to eq main_category
+      end
     end
   end
 end
