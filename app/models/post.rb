3ee2bb0e02843767c0a561600ca25fca31edd00e
Diamant <diamantkolshi@gmail.com>
2021-10-02 12:55:32 +0200

    Add new scope to filter posts by categories, add tests for all cases ..

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :project
   has_many :ingredients
+
+  scope :filter_by_categories, -> (categories_ids) {
+    select("DISTINCT ON (id) #{self.table_name}.*")
+      .includes(:ingredients)
+      .where(ingredients: { category_id: categories_ids })
+  }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,4 +8,32 @@ RSpec.describe Post, type: :model do
 
   describe "Validations" do
   end
+
+  describe 'Filters' do
+    describe '#posts_by_categories' do
+      it 'fetch post by ingredients category (DISTINCT)' do
+        posts_result = Post.filter_by_categories([6,7])
+
+        expect(posts_result.length).to eq 1
+
+        posts_result.each do |post|
+          expect(post).to eq posts(:project1_post)
+        end
+      end
+
+      it 'fetch posts with the diff categories (DISTINCT)' do
+        posts_result = Post.filter_by_categories([6,7,12])
+
+        expect(posts_result.length).to eq 2
+        expect(posts_result).to eq [Post.first, Post.second]
+      end
+
+      it 'fetch all posts with the diff categories (DISTINCT)' do
+        posts_result = Post.filter_by_categories([6,7,12,13,14,16,18,19])
+
+        expect(posts_result.length).to eq Post.all.length
+        expect(posts_result).to eq Post.all.to_a
+      end
+    end
+  end
 end
