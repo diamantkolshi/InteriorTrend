d670c9f2bbc57e471f11c3a74593ae3d88ffb219
Diamant <diamantkolshi@gmail.com>
2022-10-08 17:27:07 +0200

    add index to get posts with filters

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,7 +2,11 @@ class Api::V1::PostsController < Api::V1::BaseController
   def index
     @posts = Post.all
     @posts = @posts.filter_by_styles(params[:style_ids]) if params[:style_ids].present?
-
+    @posts = @posts.filter_by_forms(params[:form_ids]) if params[:form_ids].present?
+    @posts = @posts.filter_by_categories(params[:category_ids]) if params[:category_ids].present?
+    @posts = @posts.filter_by_colors(params[:color_ids]) if params[:color_ids].present?
+    @posts = @posts.filter_by_materials(params[:material_ids]) if params[:material_ids].present?
+    
     render json: {
       results: @posts.as_json,
       count: @posts.length
--- spec/controllers/api/v1/posts_controller_spec.rb
+++ spec/controllers/api/v1/posts_controller_spec.rb
@@ -28,6 +28,13 @@ RSpec.describe Api::V1::PostsController, type: :controller do
   let!(:category_ingredient_2) { FactoryBot.create(:category, name: "category 2") }
   let!(:category_ingredient_3) { FactoryBot.create(:category, name: "category 3") }
 
+  let!(:color1) { FactoryBot.create(:color, name: "color 1") }
+  let!(:color2) { FactoryBot.create(:color, name: "color 2") }
+  let!(:color3) { FactoryBot.create(:color, name: "color 3") }
+
+  let!(:material1) { FactoryBot.create(:material, name: "material 1") }
+  let!(:material2) { FactoryBot.create(:material, name: "material 2") }
+  let!(:material3) { FactoryBot.create(:material, name: "material 3") }
 
   let!(:ingredient1) { 
     FactoryBot.create(
@@ -60,6 +67,14 @@ RSpec.describe Api::V1::PostsController, type: :controller do
     ) 
   }
 
+  let!(:color_ingredient_1) { FactoryBot.create(:ingredient_color, ingredient: ingredient1, color: color1) }
+  let!(:color_ingredient_2) { FactoryBot.create(:ingredient_color, ingredient: ingredient2, color: color2) }
+  let!(:color_ingredient_3) { FactoryBot.create(:ingredient_color, ingredient: ingredient3, color: color3) }
+
+  let!(:material_ingredient_1) { FactoryBot.create(:ingredient_material, ingredient: ingredient1, material: material1) }
+  let!(:material_ingredient_2) { FactoryBot.create(:ingredient_material, ingredient: ingredient2, material: material2) }
+  let!(:material_ingredient_3) { FactoryBot.create(:ingredient_material, ingredient: ingredient3, material: material3) }
+
   describe "#GET index" do
     context "returns posts successfully" do
       before do 
@@ -89,6 +104,78 @@ RSpec.describe Api::V1::PostsController, type: :controller do
 
       it "return correct posts with more then one style filter" do 
         get :index, params: { style_ids: [style_ingredient_1.id, style_ingredient_2.id] }
+        @json_result = json_response_body
+        
+        expect(@json_result[:count]).to eq(2)
+        expect(@json_result[:results].as_json).to eq([post1.as_json, post2.as_json])
+      end
+    end
+
+    context "returns postes with form filter" do
+      it "return correct posts with form filter" do 
+        get :index, params: { form_ids: [form_ingredient_1.id] }
+        @json_result = json_response_body
+        
+        expect(@json_result[:count]).to eq(1)
+        expect(@json_result[:results].as_json).to eq([post1.as_json])
+      end
+
+      it "return correct posts with more then one form filter" do 
+        get :index, params: { form_ids: [form_ingredient_1.id, form_ingredient_2.id] }
+        @json_result = json_response_body
+        
+        expect(@json_result[:count]).to eq(2)
+        expect(@json_result[:results].as_json).to eq([post1.as_json, post2.as_json])
+      end
+    end
+
+    context "returns postes with category filter" do
+      it "return correct posts with category filter" do 
+        get :index, params: { category_ids: [category_ingredient_1.id] }
+        @json_result = json_response_body
+        
+        expect(@json_result[:count]).to eq(1)
+        expect(@json_result[:results].as_json).to eq([post1.as_json])
+      end
+
+      it "return correct posts with more then one category filter" do 
+        get :index, params: { category_ids: [category_ingredient_1.id, category_ingredient_2.id] }
+        @json_result = json_response_body
+        
+        expect(@json_result[:count]).to eq(2)
+        expect(@json_result[:results].as_json).to eq([post1.as_json, post2.as_json])
+      end
+    end
+
+    context "returns postes with color filter" do
+      it "return correct posts with color filter" do 
+        get :index, params: { color_ids: [color1.id] }
+        @json_result = json_response_body
+
+        expect(@json_result[:count]).to eq(1)
+        expect(@json_result[:results].as_json).to eq([post1.as_json])
+      end
+
+      it "return correct posts with more then one color filter" do 
+        get :index, params: { color_ids: [color1.id, color2.id] }
+        @json_result = json_response_body
+        
+        expect(@json_result[:count]).to eq(2)
+        expect(@json_result[:results].as_json).to eq([post1.as_json, post2.as_json])
+      end
+    end
+
+    context "returns postes with material filter" do
+      it "return correct posts with material filter" do 
+        get :index, params: { material_ids: [material1.id] }
+        @json_result = json_response_body
+
+        expect(@json_result[:count]).to eq(1)
+        expect(@json_result[:results].as_json).to eq([post1.as_json])
+      end
+
+      it "return correct posts with more then one material filter" do 
+        get :index, params: { material_ids: [material1.id, material2.id] }
         @json_result = json_response_body
         
         expect(@json_result[:count]).to eq(2)
