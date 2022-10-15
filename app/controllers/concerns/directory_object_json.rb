43f0d063e1c106c65926b5f6372c8454d1844b03
Diamant <diamantkolshi@gmail.com>
2022-10-16 00:54:18 +0200

    add postion x, y and image of ingredient post

--- app/controllers/concerns/directory_object_json.rb
+++ app/controllers/concerns/directory_object_json.rb
@@ -4,10 +4,13 @@ module DirectoryObjectJson
       id: directory.id,
       name: directory.name,
       ingredients: directory.ingredients.map do |ingredient|
-        {
-          name: ingredient.name,
+        { 
+          name: ingredient.name,  
           price: ingredient.price,
           created_at: ingredient.created_at.strftime("%FT%T"),
+          position_x: ingredient.position_x,
+          position_y: ingredient.position_y,
+          image_path: rails_blob_url(ingredient.post.image, only_path: true)
         }
       end,
       limit_price: directory.limit_price || 0,
